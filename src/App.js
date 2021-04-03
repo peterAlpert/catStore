af0842f0f4ab42629a6fdf6bee721e0e76149fdd
peter kameel <peterkameel.93@gmail.com>
2021-04-03 17:46:47 +0200

    create pages and nav

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import logo from './logo.svg';
+import {
+  BrowserRouter as Router, Switch, Route, Link
+} from "react-router-dom";
 import './App.css';
+
+//pages 
+import Home from './pages/Home'
+import Products from './pages/Products'
+import Cart from './pages/Cart'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="container">
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+          <a className="navbar-brand" href="#">MobileStore</a>
+          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav mr-auto">
+              <li className="nav-item active">
+                <Link className="nav-link" to="/">Home</Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/products">Mobiles</Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/cart">Cart</Link>
+              </li>
+            </ul>
+          </div>
+        </nav>
+
+        <Route path='/' component={Home} exact />
+        <Route path='/products' component={Products} />
+        <Route path='/cart' component={Cart} />
+
+      </div>
+    </Router>
   );
 }
 
