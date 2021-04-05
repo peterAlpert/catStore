8e4b63a28d89415b02d63e5bbc4d2a48b3cff1b8
peter kameel <peterkameel.93@gmail.com>
2021-04-05 13:51:09 +0200

    some fixes cont.

--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -3,6 +3,7 @@ import { ADD_TO_CART, REMOVE_FROM_CART, CLEAR_CART } from '../actions/types'
 function cartReducer(state, action) {
     switch (action.type) {
         case ADD_TO_CART: {
+            alert('Your product added to cart Successfully')
             return {
                 cart: [
                     ...state.cart,
@@ -12,11 +13,13 @@ function cartReducer(state, action) {
                     }
                 ]
             }
+
         }
 
         case REMOVE_FROM_CART: {
             const newCart = { ...state };
             newCart.cart.splice(action.index, 1);
+            alert('Your product deleted from cart Successfully')
             return newCart;
         }
 
